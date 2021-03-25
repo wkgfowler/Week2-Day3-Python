b8f1eea11ee74f28d5ee224ba562b116d3464dac
wkgfowler <wkgfowler@gmail.com>
2021-03-24 23:23:26 -0400

    first commit

---
+++ geomodule.py
@@ -0,0 +1,11 @@
+def square_footage(l, w):
+    area = l * w
+    return area
+
+
+from math import pi as p
+
+def circle_circumference(r):
+    circumference = 2 * p * r
+    return circumference
+
